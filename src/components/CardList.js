cd530a77eaa8bd6f7cdeb6d9c806970a025eb8b2
lawrencewong <lawrencecywong@gmail.com>
2021-12-28 18:28:14 -0500

    feat: initial commit

---
+++ src/components/CardList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {robots.map((robot, index) => {
+        return (
+          <Card
+            key={index}
+            id={robot.id}
+            name={robot.name}
+            email={robot.email}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default CardList;
